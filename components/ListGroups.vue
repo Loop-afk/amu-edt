11e049f776b02e59ed1d76cd2fe0354b3fa8348c
aaa <onemail04@gmail.com>
2021-04-30 16:37:08 +0200

    create composants schedule & ListGroups

---
+++ components/ListGroups.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>cc</div>
+        <div>ddd</div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ components/Schedule.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div>aaa</div>
+        <div>bbb</div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
