6203a50314cb92b644f3c622990be3f8ed16801f
aaa <onemail04@gmail.com>
2021-04-30 17:35:14 +0200

    fix error: __file, __ssrRegister, __compiled

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,25 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col cols="4" class="box">
-        aaa
-      </b-col>
-      <b-col cols="8" class="box">
-        bbb
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col cols="4" class="box">
+          <list-groups />
+        </b-col>
+        <b-col cols="8" class="box">
+          <schedule />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
 import ListGroups from '~/components/ListGroups.vue'
 import Schedule from '~/components/Schedule.vue'
 export default {
-  components: ListGroups, Schedule
+  components: {
+    ListGroups, Schedule
+  }
 }
 </script>
 
