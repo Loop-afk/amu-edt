8863b3ff2519caf97d96fd15056153598b4f64f6
aaa <onemail04@gmail.com>
2021-04-30 17:09:56 +0200

    clean up & component declaration

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <b-container>
-      <b-row>
-        <b-col cols="4" class="box">
-          aaa
-        </b-col>
-        <b-col cols="8" class="box">
-          bbb
-        </b-col>
-      </b-row>
+  <b-container>
+    <b-row>
+      <b-col cols="4" class="box">
+        aaa
+      </b-col>
+      <b-col cols="8" class="box">
+        bbb
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -15,7 +15,7 @@
 import ListGroups from '~/components/ListGroups.vue'
 import Schedule from '~/components/Schedule.vue'
 export default {
-  components: [ListGroups, Schedule]
+  components: ListGroups, Schedule
 }
 </script>
 
