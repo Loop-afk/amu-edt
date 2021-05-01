1664416093f092580ba6780f6de1f8fea7d87250
baptiste <aaa>
2021-05-01 12:34:07 +0000

    navigatorArrow event & eslintrc.js no-console: off

---
+++ components/Navigator/NavigatorArrow.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div @click="weekChangeLeft()">
+      #go left#
+    </div>
+    <div @click="weekChangeRight()">
+      #go right#
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    weekChangeLeft () {
+      this.$emit('weekChangeEvent', 'left')
+    },
+    weekChangeRight () {
+      this.$emit('weekChangeEvent', 'right')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,12 @@
       <b-row>
         <b-col cols="4" class="box">
           <list-groups />
+          <navigator-arrow v-on:weekChangeEvent="weekChange($event)"/>
         </b-col>
         <b-col cols="8" class="box">
-          <schedule :schedule-settings-date="day" :schedule-schedule="schedule" :schedule-height="600" />
+          <schedule :schedule-settings-date="day" 
+          :schedule-schedule="schedule" 
+          :schedule-height="600" />
         </b-col>
       </b-row>
     </b-container>
@@ -15,14 +18,30 @@
 
 <script>
 import ListGroups from '~/components/ListGroups.vue'
+import NavigatorArrow from '~/components/Navigator/NavigatorArrow.vue'
 import Schedule from '~/components/Schedule.vue'
 export default {
   components: {
-    ListGroups, Schedule
+    ListGroups, Schedule,
+    NavigatorArrow
   },
   data () {
     return {
       day: new Date()
+    }
+  },
+  methods: {
+    addDays (date, days) { // prise sur stackoverflow
+      const copy = new Date(Number(date))
+      copy.setDate(date.getDate() + days)
+      return copy
+    },
+    weekChange: function (event) {
+      let offset;
+      if (event === 'right') offset = 7
+      else if (event === 'left') offset = -7
+      else console.warn('Illegal value from event: ' + event)
+      this.day = this.addDays(this.day, offset)
     }
   },
   computed: {
