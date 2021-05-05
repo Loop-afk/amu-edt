e52e6202a9f36e27bad8fcb6b45cc41cab496546
baptiste <aaa>
2021-05-05 12:53:53 +0000

    clean up & comment & offset ordinateAxis

---
+++ pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit="userLogin">
+      <div>
+        <label>Username</label>
+        <input v-model="login.username" type="text">
+      </div>
+      <div>
+        <label>Password</label>
+        <input v-model="login.password" type="text">
+      </div>
+      <div>
+        <button type="submit">
+          Submit
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async userLogin () {
+      try {
+        const response = await this.$auth.loginWith('local', { data: this.login })
+        console.log(response)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
