1ad6c64eb85dfbcedbfb6f5c501d6a3d3e2cff79
Your Name <aaa>
2021-05-11 20:56:03 +0000

    clean up

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,13 @@
 <template>
   <div>
     Hello
-
-    <!--{{ facts }} -->
   </div>
 </template>
 
 <script>
-// import OrdinateLine from '~/components/Schedule/OrdinateLine.vue'
 
 export default {
-  /*
-  components: {
-    OrdinateLine
-  },
-  async asyncData () {
-    const response = await fetch('http://192.168.1.36:18929/schedule/')
-    const facts = await response.json()
-    console.log(facts)
-    return { facts } // Same as return  { facts: facts }
-  }, */
+
   data () {
     return { }
   },
